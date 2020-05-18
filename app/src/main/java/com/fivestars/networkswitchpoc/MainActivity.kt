9b0e74fca2931722ffc743b2295d4d1b55220cb5
Darran Kelinske <darran.kelinske@gmail.com>
2020-05-18 14:51:10 -0500

    Switch out WebViewClient when we retry a request

--- app/src/main/java/com/fivestars/networkswitchpoc/MainActivity.kt
+++ app/src/main/java/com/fivestars/networkswitchpoc/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.fivestars.networkswitchpoc
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.net.*
+import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkCapabilities.*
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.webkit.*
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fivestars.rootutil.RootUtil
@@ -15,8 +21,10 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.Response
-import java.lang.Exception
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,107 +33,39 @@ class MainActivity : AppCompatActivity() {
     private val wifiRequest: NetworkRequest = NetworkRequest.Builder().addTransportType(TRANSPORT_WIFI).build()
     private val ethernetRequest: NetworkRequest = NetworkRequest.Builder().addTransportType(
         TRANSPORT_ETHERNET).build()
-
-
+    
+    private var networkInstance: Network? = null
+    set(value) {
+        field = value
+        reload_webview_button.isEnabled = true
+    }
+    
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        reload_webview_button.isEnabled = false
+        reload_webview_button.setOnClickListener {
+            loadSpeedTest()
+        }
 
         val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         cellular_button.setOnClickListener {
             connectivityManager.requestNetwork(cellularRequest, object : ConnectivityManager.NetworkCallback() {
-                override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
-                    super.onBlockedStatusChanged(network, blocked)
-                    Log.e("darran", "blocked status changed")
-
-                }
-
-                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
-                    super.onCapabilitiesChanged(network, networkCapabilities)
-                    Log.e("darran", "capabilities changed")
-
-                }
-
-                override fun onLost(network: Network) {
-                    super.onLost(network)
-                    Log.e("darran", "lost")
-
-                }
-
-                override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
-                    super.onLinkPropertiesChanged(network, linkProperties)
-                    Log.e("darran", "properties changed")
-
-                }
-
-                override fun onUnavailable() {
-                    super.onUnavailable()
-                    Log.e("darran", "unavailable")
-
-                }
-
-                override fun onLosing(network: Network, maxMsToLive: Int) {
-                    super.onLosing(network, maxMsToLive)
-                    Log.e("darran", "losing")
-
-                }
-
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
-                    Toast.makeText(this@MainActivity, "CELLULAR READY", Toast.LENGTH_SHORT).show()
-                    Log.e("darran", "network count is: " +connectivityManager.allNetworks.size)
-                    Log.e("darran", "network is metered: " +connectivityManager.isActiveNetworkMetered)
-                    Log.e("darran", "bind is tru: " +connectivityManager.bindProcessToNetwork(network))
-                    Log.e("darran", "bound network is: " +connectivityManager.boundNetworkForProcess)
-                    GlobalScope.launch {
-                        fetchIp(network)
-                    }
-                    connectivityManager.unregisterNetworkCallback(this)
+                    postConnect(connectivityManager, network, "LTE", this)
                 }
             })
         }
 
         wifi_button.setOnClickListener {
             connectivityManager.requestNetwork(wifiRequest, object : ConnectivityManager.NetworkCallback() {
-                override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
-                    super.onBlockedStatusChanged(network, blocked)
-                }
-
-                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
-                    super.onCapabilitiesChanged(network, networkCapabilities)
-                }
-
-                override fun onLost(network: Network) {
-                    super.onLost(network)
-                }
-
-                override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
-                    super.onLinkPropertiesChanged(network, linkProperties)
-                }
-
-                override fun onUnavailable() {
-                    super.onUnavailable()
-                }
-
-                override fun onLosing(network: Network, maxMsToLive: Int) {
-                    super.onLosing(network, maxMsToLive)
-                }
-
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
-                    Toast.makeText(this@MainActivity, "WIFI READY", Toast.LENGTH_SHORT).show()
-                    Log.e("darran", "network info is: " +connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI))
-                    Log.e("darran", "network is metered: " +connectivityManager.isActiveNetworkMetered)
-                    Log.e("darran", "bind is tru: " +connectivityManager.bindProcessToNetwork(network))
-                    Log.e("darran", "bound network is: " +connectivityManager.boundNetworkForProcess)
-                    GlobalScope.launch {
-                        val ipAddress = fetchIp(network)
-                        withContext(Dispatchers.Main) {
-                            Toast.makeText(this@MainActivity, "IP is: " +ipAddress, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                    connectivityManager.unregisterNetworkCallback(this)
+                    postConnect(connectivityManager, network, "WiFi", this)
                 }
             })
         }
@@ -142,47 +82,89 @@ class MainActivity : AppCompatActivity() {
             }
 
             connectivityManager.requestNetwork(ethernetRequest, object : ConnectivityManager.NetworkCallback() {
-                override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
-                    super.onBlockedStatusChanged(network, blocked)
-                }
-
-                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
-                    super.onCapabilitiesChanged(network, networkCapabilities)
-                }
-
-                override fun onLost(network: Network) {
-                    super.onLost(network)
-                }
-
-                override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
-                    super.onLinkPropertiesChanged(network, linkProperties)
-                }
-
-                override fun onUnavailable() {
-                    super.onUnavailable()
-                }
-
-                override fun onLosing(network: Network, maxMsToLive: Int) {
-                    super.onLosing(network, maxMsToLive)
-                }
 
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
-                    Toast.makeText(this@MainActivity, "ConnectivityManager.TYPE_ETHERNET READY", Toast.LENGTH_SHORT).show()
-                    Log.e("darran", "network info is: " +connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET))
-                    Log.e("darran", "network is metered: " +connectivityManager.isActiveNetworkMetered)
-                    Log.e("darran", "bind is tru: " +connectivityManager.bindProcessToNetwork(network))
-                    Log.e("darran", "bound network is: " +connectivityManager.boundNetworkForProcess)
-                    GlobalScope.launch {
-                        val ipAddress = fetchIp(network)
-                        withContext(Dispatchers.Main) {
-                            Toast.makeText(this@MainActivity, "IP is: " +ipAddress, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                    connectivityManager.unregisterNetworkCallback(this)
+                    postConnect(connectivityManager, network, "Ethernet", this)
                 }
             })
         }
+    }
+
+    private fun loadSpeedTest() {
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldInterceptRequest(
+                view: WebView,
+                request: WebResourceRequest
+            ): WebResourceResponse? {
+                if (networkInstance != null) {
+                    try {
+                        val url = URL(request.url.toString())
+                        val connection: HttpURLConnection =
+                            networkInstance?.openConnection(url) as HttpURLConnection
+                        connection.setDoOutput(true)
+                        connection.setRequestMethod(request.method)
+                        for ((key, value) in request.requestHeaders) {
+                            connection.setRequestProperty(key, value)
+                        }
+                        val responseCode: Int = connection.getResponseCode()
+                        val responseMessage: String = connection.getResponseMessage()
+                        var contentType: String = connection.getContentType()
+                        var encoding: String? = connection.getContentEncoding()
+
+                        // Transform response headers from Map<String, List<String>> to Map<String, String>
+                        val headerFields: HashMap<String?, String?> = HashMap()
+                        connection.headerFields.map {
+                            headerFields.put(it.key, it.value[0])
+                        }
+                        val contentTypeData =
+                            contentType.split(";\\s?").toTypedArray()
+                        contentType = contentTypeData[0]
+                        if (contentTypeData.size > 1) {
+                            encoding = contentTypeData[1]
+                        }
+                        if (contentType.contains("text") && encoding == null) {
+                            encoding = "utf-8"
+                        }
+                        val inputStream: InputStream
+                        inputStream = try {
+                            connection.getInputStream()
+                        } catch (e: java.lang.Exception) {
+                            connection.getErrorStream()
+                        }
+                        return WebResourceResponse(
+                            contentType,
+                            encoding,
+                            responseCode,
+                            responseMessage,
+                            headerFields,
+                            inputStream
+                        )
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+                return super.shouldInterceptRequest(view, request)
+            }
+        }
+        val webSettings: WebSettings = webView.getSettings()
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webView.loadUrl("http://icanhazip.com")
+    }
+
+    private fun postConnect(connectivityManager: ConnectivityManager, network: Network, connectionType: String, networkCallback: ConnectivityManager.NetworkCallback) {
+        Toast.makeText(this@MainActivity, connectionType, Toast.LENGTH_SHORT).show()
+        Log.e(TAG, "network info is: $network")
+        Log.e(TAG, "network is metered: " +connectivityManager.isActiveNetworkMetered)
+        Log.e(TAG, "bind is tru: " +connectivityManager.bindProcessToNetwork(network))
+        Log.e(TAG, "bound network is: " +connectivityManager.boundNetworkForProcess)
+        GlobalScope.launch {
+            withContext(Dispatchers.Main) {
+                networkInstance = network
+            }
+        }
+        connectivityManager.unregisterNetworkCallback(networkCallback)
     }
 
     suspend fun fetchIp(network: Network): String? = withContext(Dispatchers.IO) {
@@ -197,10 +179,10 @@ class MainActivity : AppCompatActivity() {
 
             val response = client.newCall(request).execute()
             val result = response.body?.string()
-            Log.e("darran", "The ip is: $result")
+            Log.e(TAG, "The ip is: $result")
             return@withContext result
         } catch (e: Exception) {
-            Log.e("darran", "exception: $e")
+            Log.e(TAG, "exception: $e")
         }
 
         return@withContext null
