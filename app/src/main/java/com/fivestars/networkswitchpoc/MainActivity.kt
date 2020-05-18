bc8250316b0c7a02bb864a50644ea2aa144b0b9b
Darran Kelinske <49406887+darran-kelinske-fivestars@users.noreply.github.com>
2020-05-18 16:39:34 -0500

    Okhttp version (#1)
    * Add okhttp implementation
    
    * Shrink button
    
    * Remote LTE button for Lenovo Testing
    
    * remove speed test button
    
    * Full remove speed test button
    
    * Add speed test
    
    * minor speed test update
    
    Co-authored-by: Darran Kelinske <darran.kelinske@gmail.com>

--- app/src/main/java/com/fivestars/networkswitchpoc/MainActivity.kt
+++ app/src/main/java/com/fivestars/networkswitchpoc/MainActivity.kt
@@ -8,23 +8,24 @@ import android.net.NetworkCapabilities.*
 import android.net.NetworkRequest
 import android.os.Bundle
 import android.util.Log
-import android.view.ViewGroup
 import android.webkit.*
-import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fivestars.rootutil.RootUtil
+import fr.bmartel.speedtest.SpeedTestReport
+import fr.bmartel.speedtest.SpeedTestSocket
+import fr.bmartel.speedtest.inter.ISpeedTestListener
+import fr.bmartel.speedtest.model.SpeedTestError
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import okhttp3.Call
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -33,7 +34,9 @@ class MainActivity : AppCompatActivity() {
     private val wifiRequest: NetworkRequest = NetworkRequest.Builder().addTransportType(TRANSPORT_WIFI).build()
     private val ethernetRequest: NetworkRequest = NetworkRequest.Builder().addTransportType(
         TRANSPORT_ETHERNET).build()
-    
+
+
+
     private var networkInstance: Network? = null
     set(value) {
         field = value
@@ -47,7 +50,12 @@ class MainActivity : AppCompatActivity() {
         
         reload_webview_button.isEnabled = false
         reload_webview_button.setOnClickListener {
-            loadSpeedTest()
+            loadUrl("http://icanhazip.com")
+        }
+
+        speed_test.setOnClickListener {
+            speed_status.text = "Running Speed Test"
+            runUploadSpeedTest()
         }
 
         val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -56,16 +64,27 @@ class MainActivity : AppCompatActivity() {
             connectivityManager.requestNetwork(cellularRequest, object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
-                    postConnect(connectivityManager, network, "LTE", this)
+                    onConnectedToNetwork(connectivityManager, network, "LTE", this)
                 }
             })
         }
 
         wifi_button.setOnClickListener {
+            val dropEthSuccess = RootUtil.executeAsRoot("ifconfig eth0 down").first
+
+            val success = RootUtil.executeAsRoot("ifconfig wlan0 down").first
+
+            if (success) {
+                val success = RootUtil.executeAsRoot("ifconfig wlan0 up").first
+                if (!success) {
+                    return@setOnClickListener
+                }
+            }
+
             connectivityManager.requestNetwork(wifiRequest, object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
-                    postConnect(connectivityManager, network, "WiFi", this)
+                    onConnectedToNetwork(connectivityManager, network, "WiFi", this)
                 }
             })
         }
@@ -85,13 +104,48 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
-                    postConnect(connectivityManager, network, "Ethernet", this)
+                    onConnectedToNetwork(connectivityManager, network, "Ethernet", this)
                 }
             })
         }
     }
 
-    private fun loadSpeedTest() {
+    private fun runUploadSpeedTest() {
+        val speedTestSocket = SpeedTestSocket()
+
+        speedTestSocket.addSpeedTestListener(object : ISpeedTestListener {
+            override fun onCompletion(report: SpeedTestReport) {
+                // called when download/upload is complete
+                println("[COMPLETED] rate in octet/s : " + report.transferRateOctet)
+                println("[COMPLETED] rate in bit/s   : " + report.transferRateBit)
+
+                GlobalScope.launch(Dispatchers.Main) {
+                    speed_status.text =
+                        "DL Mbps : " + (report.transferRateBit.toDouble() / 1000000)
+                }
+            }
+
+            override fun onError(
+                speedTestError: SpeedTestError,
+                errorMessage: String
+            ) {
+                // called when a download/upload error occur
+            }
+
+            override fun onProgress(percent: Float, report: SpeedTestReport) {
+                // called to notify download/upload progress
+                println("[PROGRESS] progress : $percent%")
+                println("[PROGRESS] rate in octet/s : " + report.transferRateOctet)
+                println("[PROGRESS] rate in bit/s   : " + report.transferRateBit)
+            }
+        })
+
+        GlobalScope.launch(Dispatchers.IO) {
+            speedTestSocket.startDownload("http://ashburn.va.speedtest.frontier.com:8080/speedtest/random4000x4000.jpg");
+        }
+    }
+
+    private fun loadUrl(url: String) {
         webView.webViewClient = object : WebViewClient() {
             override fun shouldInterceptRequest(
                 view: WebView,
@@ -99,46 +153,26 @@ class MainActivity : AppCompatActivity() {
             ): WebResourceResponse? {
                 if (networkInstance != null) {
                     try {
-                        val url = URL(request.url.toString())
-                        val connection: HttpURLConnection =
-                            networkInstance?.openConnection(url) as HttpURLConnection
-                        connection.setDoOutput(true)
-                        connection.setRequestMethod(request.method)
-                        for ((key, value) in request.requestHeaders) {
-                            connection.setRequestProperty(key, value)
-                        }
-                        val responseCode: Int = connection.getResponseCode()
-                        val responseMessage: String = connection.getResponseMessage()
-                        var contentType: String = connection.getContentType()
-                        var encoding: String? = connection.getContentEncoding()
+                        val client = OkHttpClient.Builder().socketFactory(networkInstance!!.socketFactory).build()
 
-                        // Transform response headers from Map<String, List<String>> to Map<String, String>
-                        val headerFields: HashMap<String?, String?> = HashMap()
-                        connection.headerFields.map {
-                            headerFields.put(it.key, it.value[0])
-                        }
-                        val contentTypeData =
-                            contentType.split(";\\s?").toTypedArray()
-                        contentType = contentTypeData[0]
-                        if (contentTypeData.size > 1) {
-                            encoding = contentTypeData[1]
-                        }
-                        if (contentType.contains("text") && encoding == null) {
-                            encoding = "utf-8"
-                        }
-                        val inputStream: InputStream
-                        inputStream = try {
-                            connection.getInputStream()
-                        } catch (e: java.lang.Exception) {
-                            connection.getErrorStream()
-                        }
+                        val call: Call = client.newCall(
+                            Request.Builder()
+                                .url(request.url.toString())
+                                .build()
+                        )
+
+                        val response: Response = call.execute()
+
                         return WebResourceResponse(
-                            contentType,
-                            encoding,
-                            responseCode,
-                            responseMessage,
-                            headerFields,
-                            inputStream
+                            response.header(
+                                "content-type",
+                                "text/plain"
+                            ),  // You can set something other as default content-type
+                            response.header(
+                                "content-encoding",
+                                "utf-8"
+                            ),  // Again, you can set another encoding as default
+                            response.body?.byteStream()
                         )
                     } catch (e: IOException) {
                         e.printStackTrace()
@@ -150,11 +184,11 @@ class MainActivity : AppCompatActivity() {
         val webSettings: WebSettings = webView.getSettings()
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true
-        webView.loadUrl("http://icanhazip.com")
+        webView.loadUrl(url)
     }
 
-    private fun postConnect(connectivityManager: ConnectivityManager, network: Network, connectionType: String, networkCallback: ConnectivityManager.NetworkCallback) {
-        Toast.makeText(this@MainActivity, connectionType, Toast.LENGTH_SHORT).show()
+    private fun onConnectedToNetwork(connectivityManager: ConnectivityManager, network: Network, connectionType: String, networkCallback: ConnectivityManager.NetworkCallback) {
+        Toast.makeText(this@MainActivity, "$connectionType is ready", Toast.LENGTH_SHORT).show()
         Log.e(TAG, "network info is: $network")
         Log.e(TAG, "network is metered: " +connectivityManager.isActiveNetworkMetered)
         Log.e(TAG, "bind is tru: " +connectivityManager.bindProcessToNetwork(network))
@@ -165,27 +199,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
         connectivityManager.unregisterNetworkCallback(networkCallback)
-    }
-
-    suspend fun fetchIp(network: Network): String? = withContext(Dispatchers.IO) {
-
-        try {
-            Log.v(TAG, "IP for www.icanhazip.com is: " +network.getByName("www.icanhazip.com"))
-            val client = OkHttpClient();
-
-            val request = Request.Builder()
-                .url("http://icanhazip.com")
-                .build();
-
-            val response = client.newCall(request).execute()
-            val result = response.body?.string()
-            Log.e(TAG, "The ip is: $result")
-            return@withContext result
-        } catch (e: Exception) {
-            Log.e(TAG, "exception: $e")
-        }
-
-        return@withContext null
     }
 
     companion object {
