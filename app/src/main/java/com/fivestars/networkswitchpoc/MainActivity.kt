82dd94d5aaa3b7b9ac426f80a37c00cbe1646c72
Darran Kelinske <darran.kelinske@gmail.com>
2020-05-19 13:43:21 -0500

    Add network switch test

--- app/src/main/java/com/fivestars/networkswitchpoc/MainActivity.kt
+++ app/src/main/java/com/fivestars/networkswitchpoc/MainActivity.kt
@@ -8,7 +8,8 @@ import android.net.NetworkCapabilities.*
 import android.net.NetworkRequest
 import android.os.Bundle
 import android.util.Log
-import android.webkit.*
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fivestars.rootutil.RootUtil
@@ -17,47 +18,44 @@ import fr.bmartel.speedtest.SpeedTestSocket
 import fr.bmartel.speedtest.inter.ISpeedTestListener
 import fr.bmartel.speedtest.model.SpeedTestError
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.Call
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.IOException
 import java.io.InputStreamReader
 import java.text.DecimalFormat
-import java.util.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val cellularRequest: NetworkRequest = NetworkRequest.Builder().addTransportType(TRANSPORT_CELLULAR).build()
-    private val wifiRequest: NetworkRequest = NetworkRequest.Builder().addTransportType(TRANSPORT_WIFI).build()
+    private val cellularRequest: NetworkRequest =
+        NetworkRequest.Builder().addTransportType(TRANSPORT_CELLULAR).build()
+    private val wifiRequest: NetworkRequest =
+        NetworkRequest.Builder().addTransportType(TRANSPORT_WIFI).build()
     private val ethernetRequest: NetworkRequest = NetworkRequest.Builder().addTransportType(
-        TRANSPORT_ETHERNET).build()
+        TRANSPORT_ETHERNET
+    ).build()
 
     var pattern = "#.##"
     var decimalFormat = DecimalFormat(pattern)
     var networkReference = "wlan"
-    var startSwitch: Long = 0
+    var startSwitchTime: Long = 0
     var switchTime: Long = 0
 
     private var networkInstance: Network? = null
-    set(value) {
-        field = value
-        reload_webview_button.isEnabled = true
-        speed_test.isEnabled = true
-    }
-    
+        set(value) {
+            field = value
+            reload_webview_button.isEnabled = true
+            speed_test.isEnabled = true
+        }
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         reload_webview_button.isEnabled = false
         reload_webview_button.setOnClickListener {
             loadUrl("http://icanhazip.com")
@@ -69,17 +67,66 @@ class MainActivity : AppCompatActivity() {
             runDownloadTest()
         }
 
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        webView.webViewClient = WebViewClient()
+
+
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        switch_report_button.setOnClickListener {
+            Switch.ethernetSwitch.clear()
+            Switch.wifiSwitch.clear()
+
+
+            GlobalScope.launch {
+                startSwitchTime =
+                    withContext(Dispatchers.IO) { connectivityManager.switchNetwork(wifiRequest, connectivityManager) }
+                try {
+
+                    for (i in 1..100) {
+
+                        withContext(Dispatchers.Main) {
+                            switch_report.text = "Switch $i of 100"
+                        }
+
+                        val switchEthernetTime =
+                            withContext(Dispatchers.IO) { switchToEthernet(connectivityManager) }
+                        Switch.ethernetSwitch.add(switchEthernetTime - startSwitchTime)
+
+                        startSwitchTime = System.currentTimeMillis()
+
+                        val switchToWifiTime =
+                            withContext(Dispatchers.IO) { switchToWifi(connectivityManager) }
+                        Switch.wifiSwitch.add(switchToWifiTime - startSwitchTime)
+                        delay(1000)
+                    }
+                } catch (e: Exception) {
+                    Log.e(TAG, e.toString())
+                }
+
+
+                withContext(Dispatchers.Main) {
+                    switch_report.text =
+                        "Max wifi switch time ${Switch.wifiSwitch.max()} - Max ethernet switch time ${Switch.ethernetSwitch.max()}\n" +
+                                "Avg wifi switch time ${Switch.wifiSwitch.average()} - Max ethernet switch time ${Switch.ethernetSwitch.average()}\n" +
+                                "Min wifi switch time ${Switch.wifiSwitch.min()} - Min ethernet switch time ${Switch.ethernetSwitch.min()}"
+                }
+
+            }
+        }
 
         cellular_button.setOnClickListener {
-            startSwitch = System.currentTimeMillis()
-            connectivityManager.requestNetwork(cellularRequest, object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    onConnectedToNetwork(connectivityManager, network, "LTE", this)
-                }
-            })
+            startSwitchTime = System.currentTimeMillis()
+            connectivityManager.requestNetwork(
+                cellularRequest,
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        onConnectedToNetwork(connectivityManager, network, "LTE", this)
+                    }
+                })
         }
+
 
         wifi_button.setOnClickListener {
 
@@ -94,13 +141,15 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            startSwitch = System.currentTimeMillis()
-            connectivityManager.requestNetwork(wifiRequest, object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    onConnectedToNetwork(connectivityManager, network, "wlan", this)
-                }
-            })
+            startSwitchTime = System.currentTimeMillis()
+            connectivityManager.requestNetwork(
+                wifiRequest,
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        onConnectedToNetwork(connectivityManager, network, "wlan", this)
+                    }
+                })
         }
 
         ethernet_button.setOnClickListener {
@@ -114,15 +163,38 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            startSwitch = System.currentTimeMillis()
-            connectivityManager.requestNetwork(ethernetRequest, object : ConnectivityManager.NetworkCallback() {
+            startSwitchTime = System.currentTimeMillis()
+            connectivityManager.requestNetwork(
+                ethernetRequest,
+                object : ConnectivityManager.NetworkCallback() {
 
-                override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    onConnectedToNetwork(connectivityManager, network, "eth", this)
-                }
-            })
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        onConnectedToNetwork(connectivityManager, network, "eth", this)
+                    }
+                })
         }
+    }
+
+    suspend fun switchToEthernet(connectivityManager: ConnectivityManager): Long {
+        val success = RootUtil.executeAsRoot("ifconfig eth0 down").first
+
+        if (success) {
+            val success = RootUtil.executeAsRoot("ifconfig eth0 up").first
+        }
+        return connectivityManager.switchNetwork(ethernetRequest, connectivityManager)
+    }
+
+    suspend fun switchToWifi(connectivityManager: ConnectivityManager): Long {
+        val dropEthSuccess = RootUtil.executeAsRoot("ifconfig eth0 down").first
+
+        val success = RootUtil.executeAsRoot("ifconfig wlan0 down").first
+
+        if (success) {
+            val success = RootUtil.executeAsRoot("ifconfig wlan0 up").first
+        }
+
+        return connectivityManager.switchNetwork(wifiRequest, connectivityManager)
     }
 
     private fun runDownloadTest() {
@@ -138,11 +210,17 @@ class MainActivity : AppCompatActivity() {
                 GlobalScope.launch(Dispatchers.Main) {
 
                     PacketLoss.after = withContext(Dispatchers.IO) {
-                        Parse.parseNetworkInfo(executeCommand("ip -s -o link", "\n")!!, networkReference)
+                        Parse.parseNetworkInfo(
+                            executeCommand("ip -s -o link", "\n")!!,
+                            networkReference
+                        )
                     }
 
                     download_status.text =
-                        "DL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Packet Loss: ${PacketLoss.calculatePacketLoss(PacketLoss.before!!, PacketLoss.after!!)}"
+                        "DL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Packet Loss: ${PacketLoss.calculatePacketLoss(
+                            PacketLoss.before!!,
+                            PacketLoss.after!!
+                        )}"
 
                     runUploadTest()
                 }
@@ -161,7 +239,9 @@ class MainActivity : AppCompatActivity() {
                 println("[PROGRESS] progress : $percent%")
                 GlobalScope.launch(Dispatchers.Main) {
                     download_status.text =
-                        "DL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Progress: ${decimalFormat.format(percent)} %"
+                        "DL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Progress: ${decimalFormat.format(
+                            percent
+                        )} %"
                 }
             }
         })
@@ -170,7 +250,7 @@ class MainActivity : AppCompatActivity() {
             PacketLoss.before = Parse.parseNetworkInfo(
                 executeCommand("ip -s -o link", "\n")!!, networkReference
             )
-            speedTestSocket.startDownload("http://ashburn.va.speedtest.frontier.com:8080/speedtest/random4000x4000.jpg");
+            speedTestSocket.startDownload("http://ashburn.va.speedtest.frontier.com:8080/speedtest/random4000x4000.jpg")
         }
     }
 
@@ -211,11 +291,17 @@ class MainActivity : AppCompatActivity() {
                 GlobalScope.launch(Dispatchers.Main) {
 
                     PacketLoss.after = withContext(Dispatchers.IO) {
-                        Parse.parseNetworkInfo(executeCommand("ip -s -o link", "\n")!!, networkReference)
+                        Parse.parseNetworkInfo(
+                            executeCommand("ip -s -o link", "\n")!!,
+                            networkReference
+                        )
                     }
 
                     upload_status.text =
-                        "UL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Packet Loss: ${PacketLoss.calculatePacketLoss(PacketLoss.before!!, PacketLoss.after!!)}"
+                        "UL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Packet Loss: ${PacketLoss.calculatePacketLoss(
+                            PacketLoss.before!!,
+                            PacketLoss.after!!
+                        )}"
                     speed_test.isEnabled = true
                 }
 
@@ -233,7 +319,9 @@ class MainActivity : AppCompatActivity() {
                 println("[PROGRESS] progress : $percent%")
                 GlobalScope.launch(Dispatchers.Main) {
                     upload_status.text =
-                        "UL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Progress: ${decimalFormat.format(percent)} %"
+                        "UL Mbps : " + decimalFormat.format((report.transferRateBit.toDouble() / 1000000)) + " Mbps - Progress: ${decimalFormat.format(
+                            percent
+                        )} %"
                 }
             }
         })
@@ -243,61 +331,34 @@ class MainActivity : AppCompatActivity() {
                 executeCommand("ip -s -o link", "\n")!!,
                 networkReference
             )
-            speedTestSocket.startUpload("http://ashburn.va.speedtest.frontier.com:8080/speedtest/upload.php", 31000000);
+            speedTestSocket.startUpload(
+                "http://ashburn.va.speedtest.frontier.com:8080/speedtest/upload.php",
+                31000000
+            )
         }
     }
 
     private fun loadUrl(url: String) {
-        webView.webViewClient = WebViewClient()
-//        webView.webViewClient = object : WebViewClient() {
-//            override fun shouldInterceptRequest(
-//                view: WebView,
-//                request: WebResourceRequest
-//            ): WebResourceResponse? {
-//                if (networkInstance != null) {
-//                    try {
-//                        val client = OkHttpClient.Builder().socketFactory(networkInstance!!.socketFactory).build()
-//
-//                        val call: Call = client.newCall(
-//                            Request.Builder()
-//                                .url(request.url.toString())
-//                                .build()
-//                        )
-//
-//                        val response: Response = call.execute()
-//
-//                        return WebResourceResponse(
-//                            response.header(
-//                                "content-type",
-//                                "text/plain"
-//                            ),  // You can set something other as default content-type
-//                            response.header(
-//                                "content-encoding",
-//                                "utf-8"
-//                            ),  // Again, you can set another encoding as default
-//                            response.body?.byteStream()
-//                        )
-//                    } catch (e: IOException) {
-//                        e.printStackTrace()
-//                    }
-//                }
-//                return super.shouldInterceptRequest(view, request)
-//            }
-//        }
-        val webSettings: WebSettings = webView.getSettings()
+//        webView.webViewClient = WebViewClient()
+        val webSettings: WebSettings = webView.settings
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true
         webView.loadUrl(url)
     }
 
-    private fun onConnectedToNetwork(connectivityManager: ConnectivityManager, network: Network, connectionType: String, networkCallback: ConnectivityManager.NetworkCallback) {
-        switchTime = System.currentTimeMillis() - startSwitch
+    private fun onConnectedToNetwork(
+        connectivityManager: ConnectivityManager,
+        network: Network,
+        connectionType: String,
+        networkCallback: ConnectivityManager.NetworkCallback
+    ) {
+        switchTime = System.currentTimeMillis() - startSwitchTime
         Toast.makeText(this@MainActivity, "$connectionType is ready", Toast.LENGTH_SHORT).show()
         networkReference = connectionType
         Log.e(TAG, "network info is: $network")
-        Log.e(TAG, "network is metered: " +connectivityManager.isActiveNetworkMetered)
-        Log.e(TAG, "bind is tru: " +connectivityManager.bindProcessToNetwork(network))
-        Log.e(TAG, "bound network is: " +connectivityManager.boundNetworkForProcess)
+        Log.e(TAG, "network is metered: " + connectivityManager.isActiveNetworkMetered)
+        Log.e(TAG, "bind is tru: " + connectivityManager.bindProcessToNetwork(network))
+        Log.e(TAG, "bound network is: " + connectivityManager.boundNetworkForProcess)
         GlobalScope.launch {
             withContext(Dispatchers.Main) {
                 networkInstance = network
@@ -311,3 +372,18 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "NetworkSwitchPOC"
     }
 }
+
+
+suspend fun ConnectivityManager.switchNetwork(networkRequest: NetworkRequest, connectivityManager: ConnectivityManager): Long =
+    suspendCoroutine { continuation ->
+        requestNetwork(
+            networkRequest,
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    connectivityManager.unregisterNetworkCallback(this)
+                    continuation.resume(System.currentTimeMillis())
+                }
+            })
+    }
+
